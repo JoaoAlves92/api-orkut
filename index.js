50dd2a826e4127c184a61160f509e22dfbad15a3
JoaoAlves92 <jvamarangonia@gmail.com>
2023-03-26 20:57:47 -0300

    refresh

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
-const express = require('express');
-const User = require("./src/models/user")
+const express = require("express");
+const User = require("./src/models/user");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.static('public'))
+app.use(express.json());
+app.use(express.static("public"));
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
-app.get('/', (req, res) => {
-    return res.send('Api')
-})
+app.get("/", (req, res) => {
+  return res.send("Api");
+});
 
-app.get('/:slug', async (req, res) => {
-    const slug = req.params.slug
-    const user = await User.findOne({ slug })
-    return res.send({user})
-})
+app.get("/:slug", async (req, res) => {
+  const slug = req.params.slug;
+  const user = await User.findOne({ slug });
+  return res.send({ user });
+});
 
-require('./src/controllers/authController')(app)
+require("./src/controllers/authController")(app);
 
 app.listen(process.env.PORT || 3002, () => {
-    console.log('servidor iniciado')
-})
+  console.log("servidor iniciado");
+});
